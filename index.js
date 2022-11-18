bfff00e7fc39181887a45bac20fa9e6b8accec5d
anujan542 <anujan9300@gmail.com>
2022-11-18 16:09:30 +0530

    Added outName Function

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/render", async function (req, res) {
+  //<----- Render Video On Lambda ---->
   const { bucketName, renderId, cloudWatchLogs } = await renderMediaOnLambda({
     region: "us-east-1",
     functionName: "remotion-render-3-3-1-mem2048mb-disk2048mb-900sec",
@@ -37,8 +38,13 @@ app.get("/render", async function (req, res) {
     imageFormat: "jpeg",
     maxRetries: 1,
     privacy: "public",
+    outName: {
+      key: "7e4e730a-b28d-4be5-b357-298708ad6e7f/sample1.mp4",
+      bucketName: "remotionlambda-mym3rl12bp",
+    },
   });
 
+  //<----- Render still for Thumnail ---->
   const { estimatedPrice, url, sizeInBytes } = await renderStillOnLambda({
     region: "us-east-1",
     functionName: "remotion-render-3-3-1-mem2048mb-disk2048mb-900sec",
@@ -46,20 +52,25 @@ app.get("/render", async function (req, res) {
       "https://remotionlambda-mym3rl12bp.s3.us-east-1.amazonaws.com/sites/classicMain/index.html",
     composition: "HelloWorld",
     inputProps: { id: "7e4e730a-b28d-4be5-b357-298708ad6e7f" },
-    imageFormat: "png",
+    imageFormat: "jpeg",
     maxRetries: 1,
     privacy: "public",
     envVariables: {},
     frame: 106,
+    outName: {
+      key: "7e4e730a-b28d-4be5-b357-298708ad6e7f/sample1.jpeg",
+      bucketName: "remotionlambda-mym3rl12bp",
+    },
   });
 
-  console.log("video on lambda", bucketName, renderId, cloudWatchLogs);
-  console.log("video on lambda", estimatedPrice, url, sizeInBytes);
+  console.log("Render Video", bucketName, renderId, cloudWatchLogs);
+  console.log("Thumnail Details", estimatedPrice, url, sizeInBytes);
+
   while (true) {
     await new Promise((resolve) => setTimeout(resolve, 3000));
     const progress = await getRenderProgress({
       renderId: `${renderId}`,
-      bucketName: `${bucketName}`,
+      bucketName: `remotionlambda-mym3rl12bp`,
       functionName: "remotion-render-3-3-1-mem2048mb-disk2048mb-900sec",
       region: "us-east-1",
     });
@@ -78,10 +89,6 @@ app.get("/render", async function (req, res) {
         VideoUrl: progress.outputFile,
         Videocost: `$${progress.costs.displayCost}`,
         thumnailUrl: url,
-        thumnailCost: estimatedPrice.accruedSoFar,
-        TotalCost: `$${
-          progress.costs.displayCost + estimatedPrice.accruedSoFar
-        }`,
         TotalRenderTimeInMinutes: Math.floor(progress.timeToFinish / 60000),
       });
       process.exit(0);
